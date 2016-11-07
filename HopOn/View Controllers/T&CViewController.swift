2cb6120fa60890b7137158a317bfd81af0ca2f4e
Rohit Saraf <rsaraf@easi.com>
2016-11-07 11:36:37 +0530

    Initial Commit

---
+++ HopOn/View Controllers/T&CViewController.swift
@@ -0,0 +1,17 @@
+//
+//  T&CViewController.swift
+//  HopOn
+//
+//  Created by ROHIT SARAF on 04/11/16.
+//  Copyright © 2016 AppLives. All rights reserved.
+//
+
+import UIKit
+
+class T_CViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
---
+++ HopOn/View Controllers/WelcomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomeViewController.swift
+//  HopOn
+//
+//  Created by ROHIT SARAF on 04/11/16.
+//  Copyright © 2016 AppLives. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
